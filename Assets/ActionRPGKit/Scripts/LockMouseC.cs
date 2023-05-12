95f18c4f0d6f68a4a535b2e18262b103fb9248ef
Reyirel2790 <129154853+Reyirel2790@users.noreply.github.com>
2023-05-12 16:41:18 -0600

    Eliminación de todos los archivos

--- Assets/ActionRPGKit/Scripts/LockMouseC.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LockMouseC : MonoBehaviour {
-	void  Start (){
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-	}
-}
